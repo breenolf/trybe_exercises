8659c5a69f775156bdc72bbc0ff9518a51ef02d6
Breenolf <blf@ic.ufal.br>
2020-08-18 15:41:06 -0300

    Initial commit

---
+++ my-shell-scripts/exercicio5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#exercício 05 sobre Shell Script
+
+read -p "Digite um caminho de arquivo ou diretório: " CAMINHO
+
+if [ -d "$CAMINHO" ]
+ then
+    echo "É UM DIRETÓRIO"
+elif [ -f "$CAMINHO" ]
+ then
+    echo "É UM ARQUIVO COMUM"
+else
+    echo "É QUALQUER OUTRO TIPO DE ARQUIVO" 
+fi
+
+ls -l $CAMINHO
+
+# /home/breno/Documentos/TRYBE/vscode/pasta
