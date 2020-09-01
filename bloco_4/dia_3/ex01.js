4d319d98dd413242f75df043fab238984d067686
Breenolf <blf@ic.ufal.br>
2020-09-01 18:39:19 -0300

    Alteração exercicio 01

---
+++ bloco_4/dia_3/ex01.js
@@ -0,0 +1,11 @@
+// n > 1
+// faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+
+
+let n = 5;
+
+for (let i = 1; i <= n; i += 1) {
+    console.log("*".repeat(n));
+}
+
+// console.log(n)
