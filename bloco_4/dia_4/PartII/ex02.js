f661fada668ea23b896c8787fe164e4b17d1d04d
Breenolf <blf@ic.ufal.br>
2020-09-02 11:33:39 -0300

    Exercicio 02 parte II corrigido e refatorado

--- bloco_4/dia_4/PartII/ex02.js
+++ bloco_4/dia_4/PartII/ex02.js
@@ -1,12 +1,14 @@
 function indiceDeMaiorValor(array) {
     let maiorIndice = 0;
-// 
+    let valor = 0;
+ 
     for (let i = 0; i < array.length; i += 1) {
-        // 
-        if (array[i] > array[i-1] && array[i] > maiorIndice) {
+         
+        if (array[i] > valor) {
             maiorIndice = i;
+            valor = array[i];
         }
     }
   return maiorIndice; 
 }
-console.log(indiceDeMaiorValor([2, 300, 6, 7, 10, 100]));
+console.log(indiceDeMaiorValor([2, 3, 6, 7, 10, 1]));
