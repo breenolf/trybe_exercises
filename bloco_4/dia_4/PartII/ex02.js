34cb73e02598b5b5adf50b9d50919b3412963265
Breenolf <blf@ic.ufal.br>
2020-09-02 00:48:04 -0300

    Alteração exercício 02 Parte II

--- bloco_4/dia_4/PartII/ex02.js
+++ bloco_4/dia_4/PartII/ex02.js
@@ -1,8 +1,8 @@
 function indiceDeMaiorValor(array) {
     let maiorIndice = 0;
-
+// 
     for (let i = 0; i < array.length; i += 1) {
-        
+        // 
         if (array[i] > array[i-1] && array[i] > maiorIndice) {
             maiorIndice = i;
         }
