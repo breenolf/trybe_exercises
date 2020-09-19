28264257cc4abbbd4f4578a9e2ed46d039da5b31
Breenolf <blf@ic.ufal.br>
2020-09-19 03:17:00 -0300

    Exercicio part 1 feito

---
+++ Módulo 1/bloco_6/dia_1/script.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+    let state = document.querySelector('#state');
+    let options = document.querySelector('#option');
+
+    for(let key in options) {
+        let option = document.createElement('option')
+    }
+}
+
+const options = {
+    'AC': 'Acre',
+    'AL': 'Alagoas',
+    'AP': 'Amapá',
+    'AM': 'Amazonas',
+    'BA': 'Bahia',
+    'CE': 'Ceará',
+    'DF': 'Distrito Federal',
+    'ES': 'Espírito Santo',
+    'GO': 'Goiás',
+    'MA': 'Maranhão',
+    'MT': 'Mato Grosso',
+    'MS': 'Mato Grosso do Sul',
+    'MG': 'Minas Gerais',
+    'PA': 'Pará',
+    'PB': 'Paraíba',
+    'PR': 'Paraná',
+    'PE': 'Pernambuco',
+    'PI': 'Piauí',
+    'RJ': 'Rio de Janeiro',
+    'RN': 'Rio Grande do Norte',
+    'RS': 'Rio Grande do Sul',
+    'RO': 'Rondônia',
+    'RR': 'Roraima',
+    'SC': 'Santa Catarina',
+    'SP': 'São Paulo',
+    'SE': 'Sergipe',
+    'TO': 'Tocantins'
+}
